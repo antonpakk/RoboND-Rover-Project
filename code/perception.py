7a582e45c5e295acd7b2f4b9952a83729eca6e8c
Antonio Park <antonpark@Antonios-MacBook-Pro.local>
2017-06-06 23:13:50 +0900

    updates

--- code/perception.py
+++ code/perception.py
@@ -3,7 +3,7 @@ import cv2
 
 # Identify pixels above the threshold
 # Threshold of RGB > 160 does a nice job of identifying ground pixels only
-def color_thresh(img, rgb_thresh=(160, 160, 160)):
+def color_thresh(img, rgb_thresh=(160, 160, 160), rgb_thresh_upper=(255, 255, 255)):
     # Create an array of zeros same xy size as img, but single channel
     color_select = np.zeros_like(img[:,:,0])
     # Require that each pixel be above all three threshold values in RGB
@@ -11,7 +11,10 @@ def color_thresh(img, rgb_thresh=(160, 160, 160)):
     # where threshold was met
     above_thresh = (img[:,:,0] > rgb_thresh[0]) \
                 & (img[:,:,1] > rgb_thresh[1]) \
-                & (img[:,:,2] > rgb_thresh[2])
+                & (img[:,:,2] > rgb_thresh[2]) \
+                & (img[:,:,0] <= rgb_thresh_upper[0]) \
+                & (img[:,:,1] <= rgb_thresh_upper[1]) \
+                & (img[:,:,2] <= rgb_thresh_upper[2])
     # Index the array of zeros with the boolean array and set to 1
     color_select[above_thresh] = 1
     # Return the binary image
@@ -43,8 +46,10 @@ def rotate_pix(xpix, ypix, yaw):
     # TODO:
     # Convert yaw to radians
     # Apply a rotation
-    xpix_rotated = 0
-    ypix_rotated = 0
+    yaw_rad = yaw * np.pi / 180.0
+    xpix_rotated = (xpix * np.cos(yaw_rad)) - (ypix * np.sin(yaw_rad))
+                            
+    ypix_rotated = (xpix * np.sin(yaw_rad)) + (ypix * np.cos(yaw_rad))
     # Return the result  
     return xpix_rotated, ypix_rotated
 
@@ -52,8 +57,8 @@ def rotate_pix(xpix, ypix, yaw):
 def translate_pix(xpix_rot, ypix_rot, xpos, ypos, scale): 
     # TODO:
     # Apply a scaling and a translation
-    xpix_translated = 0
-    ypix_translated = 0
+    xpix_translated = (xpix_rot / scale) + xpos
+    ypix_translated = (ypix_rot / scale) + ypos
     # Return the result  
     return xpix_translated, ypix_translated
 
@@ -104,7 +109,60 @@ def perception_step(Rover):
         # Rover.nav_dists = rover_centric_pixel_distances
         # Rover.nav_angles = rover_centric_angles
     
- 
+    #########################################################################################################
+    #Defining source and destination points
     
+    # Define calibration box in source (actual) and destination (desired) coordinates
+    # These source and destination points are defined to warp the image
+    # to a grid where each 10x10 pixel square represents 1 square meter
+    # The destination box will be 2*dst_size on each side
+    dst_size = 5 
+    # Set a bottom offset to account for the fact that the bottom of the image 
+    # is not the position of the rover but a bit in front of it
+    # this is just a rough guess, feel free to change it!
+    bottom_offset = 6
     
+    image = Rover.img
+    source = np.float32([[14, 140], [301 ,140],[200, 96], [118, 96]])
+    destination = np.float32([[image.shape[1]/2 - dst_size, image.shape[0] - bottom_offset],
+                      [image.shape[1]/2 + dst_size, image.shape[0] - bottom_offset],
+                      [image.shape[1]/2 + dst_size, image.shape[0] - 2*dst_size - bottom_offset], 
+                      [image.shape[1]/2 - dst_size, image.shape[0] - 2*dst_size - bottom_offset],
+                      ])
+    
+    warped = perspect_transform(image, source, destination)
+    ####Apply threshold to warped img
+    warped_threshed_navigable = color_thresh(warped, rgb_thresh=(160, 160, 160))
+    warped_threshed_obstacle = color_thresh(warped, (0, 0, 0), (160, 160, 160))
+    warped_threshed_rock = color_thresh(warped, (77, 68, 0), (197, 168, 40))
+    
+    Rover.vision_image[:,:,0] = warped_threshed_obstacle
+    Rover.vision_image[:,:,1] = warped_threshed_rock
+    Rover.vision_image[:,:,2] = warped_threshed_navigable
+    
+    ####Apply transform into rover-centric coords
+    xpix_nav, ypix_nav = rover_coords(warped_threshed_navigable)
+    xpix_rock, ypix_rock = rover_coords(warped_threshed_rock)
+    xpix_obs, ypix_obs = rover_coords(warped_threshed_obstacle)
+    
+    ####Get data
+    xpos = Rover.pos[0]
+    ypos = Rover.pos[1]
+    yaw = Rover.yaw
+    world_size = Rover.worldmap.shape[0]
+    scale = 2*dst_size
+    #### 
+    x_pix_obs_world, y_pix_obs_world = pix_to_world(xpix_obs, ypix_obs, xpos, ypos, yaw, world_size, scale)
+    x_pix_nav_world, y_pix_nav_world = pix_to_world(xpix_nav, ypix_nav, xpos, ypos, yaw, world_size, scale)
+    x_pix_rock_world, y_pix_rock_world = pix_to_world(xpix_rock, ypix_rock, xpos, ypos, yaw, world_size, scale)
+    
+    Rover.worldmap[y_pix_obs_world, x_pix_obs_world, 0] += 1
+    Rover.worldmap[y_pix_rock_world, x_pix_rock_world, 1] += 1
+    Rover.worldmap[y_pix_nav_world, x_pix_nav_world, 2] += 1
+    
+    ####
+    dists, angles = to_polar_coords(xpix_nav, ypix_nav)
+    #print ('$#$#$$$#:',xpos, ypos, yaw, dists, angles)
+    Rover.nav_dists, Rover.nav_angles = dists, angles
+
     return Rover
